5095d20ab713e2221942480ffad71ea8f6a701a6
kn222gn <kn222gn@student.lnu.se>
2014-09-18 13:44:28 +0200

    första commiten för labben

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = ReadInt("Enter the amount of salaries you want to process: ");
+            int Amount = ReadInt("Enter the amount of the salaries (kr): ");
+
+        }
+        static int ReadInt(string Prompt)
+        {
+            Console.Write(Prompt);
+            int Newcount = int.Parse(Console.ReadLine());
+            return Newcount;
+        }
+    }
+}
