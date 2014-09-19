41711936bda29d8c117ab5ab74b630b9251784ce
kn222gn <kn222gn@student.lnu.se>
2014-09-19 16:33:08 +0200

    nu har jag kommit lite längre med medianlön osv

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,26 +10,79 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int count = ReadInt("Enter the amount of salaries you want to process: ");
-            int Amount = ReadInt("Enter the amount of the salaries (kr): ");
-
-            if (count <= 2)
+            do
             {
-                ProcessSalaries(count);
-            }
+                int count = ReadInt("Enter the amount of salaries you want to process: ");
+                Console.WriteLine(count);
 
+                if (count >= 2)
+                {
+                    ProcessSalaries(count);  //om nu count är mindre än 2 så kommer else att skriva ut texten med felmeddelande och be personen skriva ut antal igen som är lika med 2 eller mer!
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Not a valid amount of salaries please try again! Must be more than 2");
+                    Console.ResetColor();
+
+                    Console.WriteLine();
+
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Press any key to continue or press ESC to Close!");
+                    Console.ResetColor();
+                }
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);  // här använder vi oss av en do/while där vi avslutar med en tangent och avsluta med escape knappen!
         }
+             
+                
         static int ReadInt(string Prompt)
         {
             Console.Write(Prompt);
-            int NewValue = int.Parse(Console.ReadLine());
+            int NewValue = int.Parse(Console.ReadLine());  //här skrivs det i static void längst upp in
             return NewValue;
         }
         static void ProcessSalaries(int count)
-        { 
+        {
+            int MedianOne;
+            int MedianTwo;
+            int Median;
+            int[] Salaries = new int[count];  // nu ska personen skriva in alla löner
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("Enter salary nr  : ");
+                Salaries[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("\n");
+
+            Array.Sort(Salaries);
+
+            if (count % 2 == 0)
+            {
+                MedianOne = count / 2;
+                MedianTwo = MedianOne - 1;
+
+                Median = (Salaries[MedianOne] + Salaries[MedianTwo]) / 2;
+
+                Console.WriteLine("The median is: {0:c}", Median);
+
+            }
+            else
+            { 
+                Median = count / 2;
+                Console.WriteLine("The median is: {0:c}", Salaries[Median]);
+            }
+            
+            //foreach (int Element in i ) Console.Write(Element + " ");  //Skriver arrayen, Antal beror på inskrivet antal i count! i ordning!
+            //{
+            //    Console.WriteLine();
+            //}
 
 
-        
+
         }
     }
 }
