bec8296c34f78f7f6a6650804291279346492ede
kn222gn <kn222gn@student.lnu.se>
2014-09-23 14:47:46 +0200

    nu är den så klar som jag kan bli med den just nu

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,10 +10,13 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+
             do
             {
+                Console.Clear();
                 int count = ReadInt("Enter the amount of salaries you want to process: ");
-                Console.WriteLine(count);
+                Console.Write("\n");
+
 
                 if (count > 2)
                 {
@@ -33,73 +36,101 @@ namespace ConsoleApplication1
                     Console.WriteLine("Press any key to continue or press ESC to Close!");
                     Console.ResetColor();
                 }
+
             }
-            while (Console.ReadKey(true).Key != ConsoleKey.Escape);  // här använder vi oss av en do/while där vi avslutar med en tangent och avsluta med escape knappen!
-        }
-             
-                
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }  // här använder vi oss av en do/while där vi avslutar med en tangent och avsluta med escape knappen!
+            
+        
         static int ReadInt(string Prompt)
         {
+            
+           while(true)
+           {    
             Console.Write(Prompt);
-            int NewValue = int.Parse(Console.ReadLine());  //här skrivs det i static void längst upp in
-            return NewValue;
+            string input = Console.ReadLine();
+            try
+            {
+                int NewValue = int.Parse(input);  //Den här satsen refererar jag olika saker till den gör olika beroende på vad jag skickar till den!
+                return NewValue;
+            }
+
+            catch
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("'{0}' is in wrong format!", input);
+                Console.ResetColor();
+            }
+
+        }
+
         }
         static void ProcessSalaries(int count)
         {
-            int MedianOne;
+            int MedianOne;                    // här har vi olika variablar som underlättar när jag räknar ut medianen då jag använder de två översta i "min" och "max".
             int MedianTwo;
             int Median;
-                                              //int[] unsortedSalaries = new int(count);
+            int[] unsortedSalaries = new int[count];  // här har vi en ny array där dom osorterade lönerna ska delas upp och sparas när man kopierar!
             int number = 0;
             int[] Salaries = new int[count];  // nu ska personen skriva in alla löner
             for (int i = 0; i < count; i++)
             {
                 number += 1;
-                Salaries[i] = ReadInt("Enter Salarie " + (i+1) + (": "));
+                Salaries[i] = ReadInt("Enter Salarie NR " + (i+1) + (": "));
                 
             }
 
             Console.Write("\n");
 
-                                             //Array.Copy(Salaries, UnsortedSalaries, count);
+            Array.Copy(Salaries, unsortedSalaries, count); // kopierar arrayen så att den sparas till senare!
             Array.Sort(Salaries);
 
             Console.WriteLine("-------------------------------------------");
             if (count % 2 == 0)
             {
-                MedianOne = count / 2;
-                MedianTwo = MedianOne - 1;
+                MedianOne = count / 2;  // här delar programmet antalet löner på 2 och tar ut dom två som finns i mitten! denna sats tar ut det höga talet i mitten!
+                MedianTwo = MedianOne - 1; // sedan så tar denna sats ut det mindre talet i mitten!
 
-                Median = (Salaries[MedianOne] + Salaries[MedianTwo]) / 2;
+                Median = (Salaries[MedianOne] + Salaries[MedianTwo]) / 2; // och här i det slutliga skedet så tar vi de båda mittentalen och plussar dem för att sedan dela det på två!
 
-                Console.WriteLine("The median is: {0:c0}", Median);
+                Console.WriteLine("The median is:          {0,10:c0}", Median);
 
             }
             else
             { 
               Median = count / 2;
-              Console.WriteLine("The median Salarie is:  {0:c0}", Salaries[Median]);
+              Console.WriteLine("The median Salarie is:  {0,10:c0}", Salaries[Median]);
             }
 
               Salaries.Average();
-              Console.WriteLine("The average salarie is: {0:c0}", Salaries.Average());
+              Console.WriteLine("The average salarie is: {0,10:c0}", Salaries.Average());
            
 
-              Console.WriteLine("The diffrence is:       {0:c0} ", Salaries.Max() - Salaries.Min() );
+              Console.WriteLine("The diffrence is:       {0,10:c0} ", Salaries.Max() - Salaries.Min() );
 
 
               Console.WriteLine("-------------------------------------------");
 
-
+              for (int i = 0; i < count; i++)  // här loopar vi if satsen i en for loop för att få så många rader som behövs därför används count!
+              {
+                  if (i % 3 == 0)  // här gör vi dock att varje gång modulus blir 0 så byter den rad, vilket är var tredje gång vilket resulterar i att det är 3 på varje rad.
+                  {
+                      Console.Write("\n");
+                  }
+                  Console.Write("{0,10}", unsortedSalaries[i]);
+              }
+              Console.WriteLine();
 
             
-            //foreach (int Element in i ) Console.Write(Element + " ");  //Skriver arrayen, Antal beror på inskrivet antal i count! i ordning!
-            //{
-            //    Console.WriteLine();
-            //}
-
-
             
+              Console.Write("\n");
+              Console.BackgroundColor = ConsoleColor.Green;
+              Console.ForegroundColor = ConsoleColor.Red;
+              Console.WriteLine("Press any key for new calculation or ESC to close!");
+              Console.ResetColor();
+
+  
             }
     }
 }
